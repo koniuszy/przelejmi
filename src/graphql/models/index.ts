39b2a94df4bd8f26647b9f7d8cb049100e05a030
Michal Konczak <michal.stefan.konczak@gmail.com>
2021-01-20 00:28:24 +0100

    creating scenario models

---
+++ src/graphql/models/index.ts
@@ -0,0 +1,39 @@
+import { mutationType, objectType, queryType } from '@nexus/schema'
+
+export const Query = queryType({
+  definition(t) {
+    t.crud.scenarios()
+    t.crud.scenario()
+  },
+})
+
+export const Mutation = mutationType({
+  definition(t) {
+    t.crud.createOneScenario()
+    t.crud.updateOneScenario()
+  },
+})
+
+export const Scenario = objectType({
+  name: 'Scenario',
+  definition(t) {
+    t.model.id()
+    t.model.name()
+    t.model.paymentType()
+    t.model.currency()
+    t.model.netPerOne()
+    t.model.VAT()
+    t.model.totalGross()
+    t.model.order()
+    t.model.description()
+    t.model.amount()
+    t.model.unit()
+    t.model.notes()
+    t.model.createdAt()
+    t.model.updatedAt()
+    t.model.buyer()
+    t.model.buyerId()
+    t.model.sellerId()
+    t.model.Seller()
+  },
+})
--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -1,45 +1,12 @@
 import path from 'path'
 
-import { objectType, queryType, makeSchema } from '@nexus/schema'
+import { makeSchema } from '@nexus/schema'
 import { nexusPrisma } from 'nexus-plugin-prisma'
 
-const Query = queryType({
-  definition(t) {
-    t.list.field('allScenarios', {
-      type: 'Scenario',
-      resolve(_parent, _args, ctx) {
-        return ctx.prisma.scenario.findMany({})
-      },
-    })
-  },
-})
-
-const Scenario = objectType({
-  name: 'Scenario',
-  definition(t) {
-    t.model.id()
-    t.model.name()
-    t.model.paymentType()
-    t.model.currency()
-    t.model.netPerOne()
-    t.model.VAT()
-    t.model.totalGross()
-    t.model.order()
-    t.model.description()
-    t.model.amount()
-    t.model.unit()
-    t.model.notes()
-    t.model.createdAt()
-    t.model.updatedAt()
-    t.model.buyer()
-    t.model.buyerId()
-    t.model.sellerId()
-    t.model.Seller()
-  },
-})
+import * as types from 'src/graphql/models'
 
 const schema = makeSchema({
-  types: [Query, Scenario],
+  types,
   //@ts-ignore
   plugins: [nexusPrisma({ experimentalCRUD: true })],
   outputs: {
