7063a27ed3c6a2de5f2920585864c9f744958cbc
Michal Konczak <michal.stefan.konczak@gmail.com>
2021-02-02 18:43:54 +0100

    adding paginatedMerchantList query

---
+++ src/graphql/models/paginatedMerchantListQuery.ts
@@ -0,0 +1,32 @@
+import { objectType, intArg, arg, extendType } from 'nexus'
+
+export const PaginatedMerchants = objectType({
+  name: 'PaginatedMerchants',
+  definition(t) {
+    t.int('totalCount')
+    t.list.field('list', { type: 'Merchant' })
+  },
+})
+
+export const PaginatedClientListQuery = extendType({
+  type: 'Query',
+  definition(t) {
+    t.crud.merchants({ ordering: true, filtering: true })
+    t.field('paginatedMerchants', {
+      type: 'PaginatedMerchants',
+      args: {
+        take: intArg({ required: true }),
+        skip: intArg({ required: true }),
+        where: arg({ type: 'MerchantWhereInput' }),
+        orderBy: arg({ type: 'MerchantOrderByInput', list: true }),
+      },
+      async resolve(_root, variables, { prisma }) {
+        const [totalCount, list] = await Promise.all([
+          prisma.merchant.count({ where: variables.where }),
+          prisma.merchant.findMany(variables),
+        ])
+        return { totalCount, list }
+      },
+    })
+  },
+})
