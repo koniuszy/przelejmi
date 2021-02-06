211dbb84a760bcffc29f813bbab7cf2267e1e190
Michal Konczak <michal.stefan.konczak@gmail.com>
2021-02-06 10:34:39 +0100

    fixing types

--- src/lib/prismaClient.ts
+++ src/lib/prismaClient.ts
@@ -2,6 +2,14 @@ import { PrismaClient } from '@prisma/client'
 
 let prisma: PrismaClient
 
+export interface Global {
+  document: Document
+  window: Window
+  prisma: PrismaClient
+}
+
+declare let global: Global
+
 if (process.env.NODE_ENV === 'production') {
   prisma = new PrismaClient()
 } else {
