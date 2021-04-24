54898ea31f504c382d3952c15b87e86112e8fb45
Michal Konczak <michal.stefan.konczak@gmail.com>
2021-04-24 20:28:39 +0200

    setting chunk analyzer

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
-module.exports = {
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+
+module.exports = withBundleAnalyzer({
   reactStrictMode: true,
   future: {
     webpack5: true,
   },
   productionBrowserSourceMaps: true,
-}
+})
