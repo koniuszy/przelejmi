1e799ad1fb72d248e5163d2183ee3b4d3aa7ca82
Michal Konczak <michal.stefan.konczak@gmail.com>
2021-12-26 12:03:44 +0100

    navigate to the last visited page after  loging in

--- src/pages/api/auth/[...auth0].ts
+++ src/pages/api/auth/[...auth0].ts
@@ -1,3 +1,17 @@
-import { handleAuth } from '@auth0/nextjs-auth0'
+import { handleAuth, handleLogin } from '@auth0/nextjs-auth0'
 
-export default handleAuth()
+export default handleAuth({
+  async login(req, res) {
+    try {
+      await handleLogin(req, res, {
+        getLoginState(req) {
+          return {
+            returnTo: req.headers.referer,
+          }
+        },
+      })
+    } catch (err: any) {
+      res.status(err.status ?? 500).end(err.message)
+    }
+  },
+})
